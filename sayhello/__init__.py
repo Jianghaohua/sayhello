e3e6868367266522dbf14f2d8a224c513f3f806f
蒋浩华 <chiang@xway.cn>
2020-07-16 14:49:06 +0800

    no message

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -9,8 +9,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-
-app = Flask('sayhello')
+app = Flask('sayhello',instance_relative_config=True)
 app.config.from_pyfile('settings.py')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
